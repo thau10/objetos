d9916e10ab970d445e29639e8abf0048cb835595
Thauane Lourenco <128156748+thau10@users.noreply.github.com>
2024-06-10 14:05:18 -0300

    Add files via upload

---
+++ objetos-main/objetos/aula02/listaObjetos.js
@@ -0,0 +1,24 @@
+const cliente ={
+    nome:"Thauane",
+    idade: 17,
+    email:"thaaune.lourenco@escola.pr.gov.br",
+    telefone:["43996165755", " 43996432555"],
+
+};
+
+cliente.enderenços =[
+    {
+    rua:"R. José Luiz Vozni",
+    numero:03,
+    apartaento: false,
+    casa:true,
+    complemento: "Perto do antigo posto de saúde",
+},
+];
+console.log(cliente.enderenços.push{
+    rua:"Lourenço",
+    numero:300,
+    apartaento: false,
+    casa:true,
+    complemento: "Perto do Copacabana Palace",
+})
---
+++ objetos-main/objetos/aula02/tipo.js
@@ -0,0 +1,8 @@
+const cliente ={
+    nome:"Thauane",
+    idade: 17,
+    email:"thaaune.lourenco@escola.pr.gov.br",
+    telefone:["43996165755", " 43996432555"],
+
+};
+console.log(cliente.telefone);
