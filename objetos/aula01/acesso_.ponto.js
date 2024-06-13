49e2be8f5648d30dec2718b9174cb7b7c85ecc82
Thauane Lourenco <128156748+thau10@users.noreply.github.com>
2024-06-13 08:43:50 -0300

    Delete objetos directory

--- objetos/aula01/acesso_.ponto.js
+++
@@ -1,9 +0,0 @@
-const objetoPessoa ={
-    nome: "Thauane",
-    idade: 17,
-    cpf: " 6792344123142",
-    email:"thauane.lourenco@escola.pr.gov.br",
-
-}
-
-console.log( objetoPessoa)
--- objetos/aula01/alterar.js
+++
@@ -1,8 +0,0 @@
-const pessoa = {
-    nome: "Aline",
-    profissao:"Biomedica",
-};
-
-console.log(pessoa.telefone);
-pessoa.telefone = "43996700157",
-console.log(pessoa.telefone);
